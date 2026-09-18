06a261c44739b66189d873209deb9f2de29e5be1
Ivan Hollow <itworkhollow@gmail.com>
2026-09-18 08:20:00 +0300

    chore: update gitignore

--- practice/24-control-student/main.go
+++ practice/24-control-student/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 type Student struct {
 	Name  string
 	Age   int
@@ -25,37 +21,4 @@ func isAdult(student Student) bool {
 	return false
 }
 
-func SetGrade(student *Student, grade int) {
-	if grade < 1 || grade > 5 {
-		return fmt.Errorf("Неверная оценка")
-	}
-	student.Grade = grade
-	return nil
-}
-
-func Status(student Student) string {
-	if student.Grade == 5 {
-		return "Отличник"
-	} else if student.Grade == 4 {
-		return "Хорошист"
-	}
-	return "Обычный студент"
-}
-
-func main() {
-	var name string
-	var age int
-	var grade int
-
-	fmt.Scan(&name, &age, &grade)
-	student := createStudent(name, age, grade)
-
-	setGrade := SetGrade(student, grade)
-	if setGrade != nil {
-		fmt.Println("Ошибка:", setGrade)
-		return
-	}
-	fmt.Println(setGrade)
-}
 //сохраняю работу как она есть, чтобы позднее проработать ошибки
-
