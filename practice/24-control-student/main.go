0187be70d42a11a722ffaaa6b950558509de7cf6
Ivan Hollow <itworkhollow@gmail.com>
2026-09-18 08:22:24 +0300

    docs: update learning roadmap

--- practice/24-control-student/main.go
+++ practice/24-control-student/main.go
@@ -6,19 +6,4 @@ type Student struct {
 	Grade int
 }
 
-func createStudent(name string, age, grade int) Student {
-	return Student{
-		Name:  name,
-		Age:   age,
-		Grade: grade,
-	}
-}
-
-func isAdult(student Student) bool {
-	if student.Age >= 18 {
-		return true
-	}
-	return false
-}
-
 //сохраняю работу как она есть, чтобы позднее проработать ошибки
