2eb3c9f6e27e14f4135f3ec912915e9e029f93ca
Ivan Hollow <itworkhollow@gmail.com>
2026-09-21 01:37:17 +0300

    feat: practice slices

--- practice/25-arrays-slices/main.go
+++ practice/25-arrays-slices/main.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [5]int{}
-	numbers[0] = 10
-	numbers[3] = 40
+	numbers := []int{}
+
+	numbers = append(numbers, 10)
+	numbers = append(numbers, 20)
+	numbers = append(numbers, 30)
+	numbers = append(numbers, 40)
+	numbers = append(numbers, 50)
+
 	for i := 0; i < len(numbers); i++ {
 		fmt.Println(numbers[i])
 	}
-	fmt.Println("Количество элементов: ", len(numbers))
+
+	fmt.Println("Количество элементов:", len(numbers))
+
+	numbers[2] = 300
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println("После изменения: ", numbers[i])
+	}
 }
