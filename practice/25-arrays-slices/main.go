b9fb4dee2e08ca05c753cee3e61520f67733b654
Ivan Hollow <itworkhollow@gmail.com>
2026-09-21 01:52:40 +0300

    feat: complete arrays and slices basics

--- practice/25-arrays-slices/main.go
+++ practice/25-arrays-slices/main.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{}
 
-	numbers = append(numbers, 10)
-	numbers = append(numbers, 20)
-	numbers = append(numbers, 30)
+	numbers := make([]int, 3, 5)
 
+	fmt.Println(numbers)
 	fmt.Println(len(numbers))
 	fmt.Println(cap(numbers))
+
+	numbers[0] = 10
+	numbers[1] = 20
+	numbers[2] = 30
 
 	numbers = append(numbers, 40)
 	numbers = append(numbers, 50)
 
+	fmt.Println(numbers)
 	fmt.Println(len(numbers))
-	fmt.Println(cap(numbers))
+	fmt.Println(len(numbers))
 }
