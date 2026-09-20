73b171c096e1ceffb5790037f15868fdc9d46cd4
Ivan Hollow <itworkhollow@gmail.com>
2026-09-21 01:45:16 +0300

    feat: practice slice capacity

--- practice/25-arrays-slices/main.go
+++ practice/25-arrays-slices/main.go
@@ -8,18 +8,13 @@ func main() {
 	numbers = append(numbers, 10)
 	numbers = append(numbers, 20)
 	numbers = append(numbers, 30)
+
+	fmt.Println(len(numbers))
+	fmt.Println(cap(numbers))
+
 	numbers = append(numbers, 40)
 	numbers = append(numbers, 50)
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
-	}
-
-	fmt.Println("Количество элементов:", len(numbers))
-
-	numbers[2] = 300
-
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println("После изменения: ", numbers[i])
-	}
+	fmt.Println(len(numbers))
+	fmt.Println(cap(numbers))
 }
