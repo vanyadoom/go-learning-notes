a7b3555d45ee5b9e3b08c665c7a9e5c760bd92f1
Ivan Hollow <itworkhollow@gmail.com>
2026-09-26 13:46:42 +0300

    finish string search practice

--- practice/34-string-search/main.go
+++ practice/34-string-search/main.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var text string
 
+	var s string
+
 	fmt.Scan(&text)
+	fmt.Scan(&s)
 
 	for i, r := range text {
-		if r == 'Я' {
+		if r == []rune(s)[0] {
 			fmt.Println(i)
+			fmt.Println("нашёл")
 		}
 	}
 }
